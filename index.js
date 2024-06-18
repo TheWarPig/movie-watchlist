54267ec76a9c3ddc63c4091b2d89d4346917781b
Ido Strassberg <idostrassberg123@gmail.com>
2024-06-18 12:36:33 +0300

    Indication when no movie were found

--- index.js
+++ index.js
@@ -7,70 +7,80 @@ let moviesHtml = ''
 async function search(movieName){
     const response = await fetch(`https://www.omdbapi.com/?apikey=e4b359c9&s=${movieName}`)
     const data = await response.json()
-    const moviesArray = data.Search
-    console.log(moviesArray)
-    
-    const fetchMovies = moviesArray.map(movie => {
-        return fetch(`https://www.omdbapi.com/?apikey=e4b359c9&i=${movie.imdbID}&type=movie&plot=short`)
-            .then(response => response.json())
-            .then(data => {
-                moviesHtml += `
-                    <div class="movie">
-                        <div class="poster">
-                            <img
-                                src="${data.Poster}"
-                                onerror="this.src='fallback.jpg'"
-                                alt="poster of the movie ${data.Title}"
-                            />
-                        </div>
-                        <div class="movie-info">
-                            <div class="movie-header">
-                                <h2>${data.Title}</h2>
-                                <i class="fa-solid fa-star"></i>
-                                <p class="rating">${data.imdbRating}</p>
+    if(!data.Error){
+        const moviesArray = data.Search    
+        const fetchMovies = moviesArray.map(movie => {
+            return fetch(`https://www.omdbapi.com/?apikey=e4b359c9&i=${movie.imdbID}&type=movie&plot=short`)
+                .then(response => response.json())
+                .then(data => {
+                    moviesHtml += `
+                        <div class="movie">
+                            <div class="poster">
+                                <img
+                                    src="${data.Poster}"
+                                    onerror="this.src='fallback.jpg'"
+                                    alt="poster of the movie ${data.Title}"
+                                />
                             </div>
-                            <div class="meta-info">
-                                <p>${data.Runtime}</p>
-                                <p>${data.Genre}</p>
-                                <p class="watchlist-div" id="watchlist-div" data-movieid=${data.imdbID} data-movie-name="${data.Title}">
-                                    <i class="fa-solid fa-circle-plus"></i>
-                                    Watchlist
+                            <div class="movie-info">
+                                <div class="movie-header">
+                                    <h2>${data.Title}</h2>
+                                    <i class="fa-solid fa-star"></i>
+                                    <p class="rating">${data.imdbRating}</p>
+                                </div>
+                                <div class="meta-info">
+                                    <p>${data.Runtime}</p>
+                                    <p>${data.Genre}</p>
+                                    <p class="watchlist-div" id="watchlist-div" data-movieid=${data.imdbID} data-movie-name="${data.Title}">
+                                        <i class="fa-solid fa-circle-plus"></i>
+                                        Watchlist
+                                    </p>
+                                </div>
+                                <p class="movie-desc">
+                                    ${data.Plot}
                                 </p>
                             </div>
-                            <p class="movie-desc">
-                                ${data.Plot}
-                            </p>
                         </div>
-                    </div>
-                `
-            })
-    })
-
-    await Promise.all(fetchMovies)
-    movieContainer.innerHTML = moviesHtml
-    document.querySelectorAll('.watchlist-div').forEach(node => {
-        node.addEventListener('click', (e) =>{
-            const movieId = e.target.dataset.movieid
-            const movieName = e.target.dataset.movieName
-            // const moveName = e.target.
-            let watchlist = getWatchlist()
-
-            // Add the movieId to the watchlist array if it's not already in there
-            if (!watchlist.includes(movieId)) {
-                watchlist.push(movieId)
-                alert(`${movieName} was added to the watchlist`)
-            }
-            else {
-                alert(`${movieName} is already in the watchlist`)
-            }
-
-            // Save the updated watchlist back to local storage
-            saveWatchlist(watchlist)
-
-            
-            
+                    `
+                })
         })
-    })
+
+        await Promise.all(fetchMovies)
+        movieContainer.innerHTML = moviesHtml
+        document.querySelectorAll('.watchlist-div').forEach(node => {
+            node.addEventListener('click', (e) =>{
+                const movieId = e.target.dataset.movieid
+                const movieName = e.target.dataset.movieName
+                // const moveName = e.target.
+                let watchlist = getWatchlist()
+
+                // Add the movieId to the watchlist array if it's not already in there
+                if (!watchlist.includes(movieId)) {
+                    watchlist.push(movieId)
+                    alert(`${movieName} was added to the watchlist`)
+                }
+                else {
+                    alert(`${movieName} is already in the watchlist`)
+                }
+
+                // Save the updated watchlist back to local storage
+                saveWatchlist(watchlist)
+
+                
+                
+            })
+        })
+        }
+
+        movieContainer.innerHTML = `
+            <div class="film-icon-placeholder" id="film-icon-placeholder">
+                <div class="no-movies">
+                    <h2>Opps! No movies were found</h2>
+                    <i class="fa-regular fa-face-frown-open"></i>
+                </div>
+            </div>
+        `
+    
 }
         
 // Function to retrieve watchlist from local storage or create an empty array if it doesn't exist
@@ -92,6 +102,10 @@ searchForm.addEventListener('submit', (e) => {
         movieContainer.innerHTML = ''
         search(searchBox.value)
         
+    }
+    else{
+        alert("Please enter a valid movie name.")
+        searchBox.value = ""
     }
     
 })
